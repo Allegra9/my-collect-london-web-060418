eb13f72641f567ebf7c6824517a44498dcf821cb
Allegra <aukseallegra@gmail.com>
2018-05-29 15:30:09 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,11 +1,11 @@
-def my_collect(array)
-  i = 0
-  while i < array.size
-    yield[i]
-    i += 1
-  end
-  array
-end 
+# def my_collect(array)
+#   i = 0
+#   while i < array.size
+#     yield[i]
+#     i += 1
+#   end
+#   array
+# end 
 
 def my_collect(array)
     new_array = []
